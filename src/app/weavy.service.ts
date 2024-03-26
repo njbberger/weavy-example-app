349a017be6e312a61f24a9692c64df6c07ece8ff
njbberger <nberger@mysourcenow.com>
2024-03-26 17:55:08 -0300

    agregado del endpoint de autenticación en backend

--- src/app/weavy.service.ts
+++ src/app/weavy.service.ts
@@ -8,8 +8,12 @@ export class WeavyService implements OnDestroy {
   weavy = new Weavy();
 
   constructor() {
-    this.weavy.url = new URL("https://myenvironment.weavy.io");
-    this.weavy.tokenUrl = new URL("https://myserver.test/api/token");
+    //this.weavy.url = new URL("https://myenvironment.weavy.io");
+    this.weavy.url = new URL("https://f075cd1672b24d388bbec498ebe7e762.weavy.io");
+
+    //this.weavy.tokenUrl = new URL("https://myserver.test/api/token");
+    this.weavy.tokenUrl = new URL("https://localhost:7126/api/token?refresh=true");
+    this.weavy.tokenUrl = new URL("https://localhost:7126/api/token?UserName=njb&Statum=0&EmailAddress=norbi88njb%40gmail.com&DisplayName=Norber&Password=Berger&OrgRoleName=rol&refresh=false");
   }
 
   ngOnDestroy(): void {
